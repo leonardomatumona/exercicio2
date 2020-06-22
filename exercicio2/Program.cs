f93e45fe1e1b14eb82951ff598e157fc0530fefb
leonardomatumona <leonardomatumona@gmail.com>
2020-06-22 15:29:16 -0300

    Add project files.

---
+++ exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = 0;
+            Console.WriteLine("digite o valor do raio:");
+            raio = double.Parse(Console.ReadLine());
+            Console.WriteLine("a área do circulo é:{0}", Math.PI * Math.Pow(2, raio));
+            Console.ReadKey();
+        }
+    }
+}
